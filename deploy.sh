4281dc49c38b6f992c6ee9ee4c8eee2eea957759
limbostray <1494255412@qq.com>
2022-04-23 16:55:47 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,16 @@
 #!/bin/zsh
+if [ ! $1 ]
+then
+  echo "####### 请输入commit值 #######"
+  exit;
+fi
 ./translate.sh
 hugo
 hugo-algolia -s
 rm -rf content/posts/*.en.md
+git add .
+git status
+sleep 1s
+git commit -m "$1"
+sleep 0.5s
+git push origin main
